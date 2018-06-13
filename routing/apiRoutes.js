f2042241a8105ee3ea6ee6974988907eacd1152b
aprilmariewilson <aprilmariewilson@yahoo.com>
2018-06-13 16:25:46 -0600

    attempted fix at value capture

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,46 +1,72 @@
-var animalData = require('../data/friends.js');
-var path = require('path');
+var bodyParser = require("body-parser");
+var path = require("path");
+var animalArray = require("../data/friends.js")
 
-var totalDifference = 0;
+
+
 
 module.exports = function (app) {
-	app.get('/api/friends', function (req, res) {
-		res.json(animalData);
-	});
-	app.post('/api/friends', function (req, res) {
 
-		var greatMatch = {
-			name: "",
-			image: "",
-			matchDifference: 1000
+	// allows express to use different data points
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	app.get("/api/friends", function (req, res) {
+		return res.json(animalArray);
+	});
+
+	// when the api/run is queried
+	app.post("/api/friends", function (req, res) {
+		// save the user info as a variable
+		var newUser = req.body;
+		var userVal = 0;
+		var compareValue = 52;
+		var friendSuggestion = {};
+// update the value for userVal
+		for (var x = 0; x < newUser.scores.length; x++){
+			userVal += parseInt(newUser.scores[x]);
+			// let's update the values on the object array to numbers so we can use them later
+			newUser.scores[x] = parseInt(newUser.scores[x])
 		};
-		var usrData = req.body;
-		var usrName = usrData.name;
-		var usrImage = usrData.image;
-		var usrScores = usrData.scores;
+		// if the values come in blank from the user for name we assign it something
+		if (newUser.name === ''){
+			newUser.name = "Frank"
+		} else {
+			console.log("The user used the name field")};
+			if (newUser.photo === ''){
+				newUser.photo = "http://www.desicomments.com/wp-content/uploads/White-Ferret-Image-desi29.jpg"
+			} else {
+				console.log("The user used the PHOTO field")};
 
-		var totalDifference = 0;
+		console.log("newUser", newUser);
+		// function that will allow us to return on a single line of code but still update values with 2 passed through variables
+		function updateValues(bestMatch, bestMatchValue) {
+			friendSuggestion = bestMatch;
+			compareValue = bestMatchValue;
+		};
+		// add up the values in the answers from the newUser
 
-		for (var i = 0; i < [animalData].length - 1; i++) {
-			console.log(animalData[i].name);
-			totalDifference = 0;
-
-			for (var j = 0; j < 11; j++) {
-
-				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(animalData[i].scores[j]));
-
-				if (totalDifference <= greatMatch.friendDifference) {
-
-					// Reset the bestMatch to be the new friend. 
-					greatMatch.name = animalData[i].name;
-					greatMatch.photo = animalData[i].photo;
-					greatMatch.matchDifference = totalDifference;
-				}
-			}
-		}
-
-		friends.push(usrData);
-
-		res.json(greatMatch);
+		// for loop to run through the information from friends.js 
+		for (var i = 0; i < animalArray.length; i++) {
+			var mA = animalArray[i];
+			console.log(mA);
+			var mAScore = 0;
+			// add all the values from the answers to the mAscore
+			for (var j = 0; j < mA.scores.length; j++) {
+				mAScore += mA.scores[j];
+				console.log(mAScore);
+			};
+			// if the value in the iteration is smaller than the current value of compareValue, then update Compare Value and the closestName to be the data from the object in the iteration
+			var difference = Math.abs(mAScore - userVal)
+			if ( difference < compareValue){
+				updateValues(mA, difference);
+			} else {console.log("This wasn't the best match for you")}
+			// else meh
+		};
+		// append the monsterMatchmaker Array with the information you just put in.
+		animalArray.push(newUser);
+// return some stuff:
+res.json({name:friendSuggestion.name, photo: friendSuggestion.photo, score: difference });
 	});
+
+
 };
